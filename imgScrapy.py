d355e9d5f722f05c3b6a7816cc483895d7ae6076
ChenKS <42082890+ChenKS12138@users.noreply.github.com>
2018-10-02 13:13:27 +0800

    模块化

--- imgScrapy.py
+++ imgScrapy.py
@@ -8,40 +8,60 @@ import os
 import time
 
 sys.stdin.encoding
-path=os.path.abspath('.')
-keyword=input("请输入想要爬取的表情包的名字  ")
-newpath=path+'\\img\\'+keyword
-if(os.path.exists(newpath)==False):
-    os.makedirs(path+'\\img\\'+keyword)
-qkeyword=urllib.parse.quote(keyword)
-page="&page=2"
-search='search?type=photo&more=1&keyword='
-url="http://www.doutula.com/"
-link=url+search+qkeyword
-req=request.Request(link)
-req.add_header('user-agent','Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/67.0.3396.99 Safari/537.36')
-text=request.urlopen(req).read()
-text=text.decode('utf-8')
-pattern1='data-original="(http|https):(/|\w|\.)+(gif|jpg|png)"'
-match=re.finditer(pattern1,text)
-f=open('match.txt','w+')
-f.write('\n')
+
+def filename(keyword):
+    path=os.path.abspath('.')
+    newpath=path+'\\img\\'+keyword
+    if(os.path.exists(newpath)==False):
+        os.makedirs(path+'\\img\\'+keyword)
+    return newpath
+
+def link(keyword,pagenum):
+    qkeyword=urllib.parse.quote(keyword)
+    page="&page=2"
+    search='search?type=photo&more=1&keyword='
+    url="http://www.doutula.com/"
+    link=url+search+qkeyword
+    req=request.Request(link)
+    req.add_header('user-agent','Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/67.0.3396.99 Safari/537.36')
+    text=request.urlopen(req).read()
+    text=text.decode('utf-8')
+    return text
+
+def match_one(text):
+    pattern1='data-original="(http|https):(/|\w|\.)+(gif|jpg|png)"'
+    match=re.finditer(pattern1,text)
+    return match
+
+
+page=2
+x=0
 totolnum=0
+
+
+keyword=input("请输入想要爬取的表情包的名字  ")
+newpath=filename(keyword)
+text=link(keyword,page)
+match=match_one(text)
+
+f=open('match.txt','w+')
 for i in match:
     f.write(i.group())
     f.write('\n')
     totolnum+=1
+f.close()
 f=open('match.txt','r+')
 str=f.read()
 pattern2='(http|https):(/|\w|\.)+(gif|jpg|png)'
 src=re.finditer(pattern2,str)
-x=0
 for i in src:
-    urllib.request.urlretrieve(i.group(),newpath+'\\'+'%s.jpg' %x)
+    urllib.request.urlretrieve(i.group(),newpath+'\\'+'%s.jpg' %(x+1))
     x+=1
     print('正在爬取%d/%d' %(x,totolnum))
+    time.sleep(0.8)
 f.close()
-if(totolnum==0):
+
+if totolnum==0:
     print('对不起,找不到关于%s的表情包,请重新输入' %keyword)
 else:
     print('爬取完成,爬取%d个文件\n保存在%s' %(totolnum,newpath))
